d32571b7916d5f32e975cb38c2f25735959cb691
donggyun-han <hdg5639@daum.net>
2025-03-01 22:19:16 +0900

    feat(Auth, Schedules): role 기반 요청 검열 기능 추가, 스케쥴 생성 구현 중
    - 토큰에 role 정보 추가
    - 특정 경로 요청에 role 식별, 제한 추가
    - 스케쥴 생성 구현 중

--- Att-Server/src/main/java/com/likelion/attserver/Service/Schedules/SchedulesService.java
+++ Att-Server/src/main/java/com/likelion/attserver/Service/Schedules/SchedulesService.java
@@ -2,8 +2,6 @@ package com.likelion.attserver.Service.Schedules;
 
 import com.likelion.attserver.DTO.SchedulesDTO;
 
-import java.util.Map;
-
 public interface SchedulesService {
     void createSchedule(Long teamId, SchedulesDTO schedule);
 }
