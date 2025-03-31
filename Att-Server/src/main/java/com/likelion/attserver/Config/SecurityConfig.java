f6c8feefff7b6bbff6b206991e68255aad3a0646
donggyun-han <hdg5639@daum.net>
2025-03-31 11:28:17 +0900

    feat(Schedule): 스케쥴 생성 로직 변경
    - List 형식으로 수신하여 일괄 등록 가능
    - 중복 여부 판단하여 제외하고 추가 가능
    - 프론트님께서 이제서야 원하신다 하셔서 오늘이 마감일인데 변경함
    - 회장 탄핵 마려워짐

--- Att-Server/src/main/java/com/likelion/attserver/Config/SecurityConfig.java
+++ Att-Server/src/main/java/com/likelion/attserver/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.likelion.attserver.Config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
